9f1825643e6a8af17d264b2f304dcce11ef47782
Burak Güner <burak.guner@callstack.com>
2023-08-15 18:07:02 +0300

    refactor: move player rendering logic inside the player component

--- src/components/GameCanvas.tsx
+++ src/components/GameCanvas.tsx
@@ -1,26 +1,13 @@
-import { Canvas, Rect, Selector } from '@shopify/react-native-skia'
+import { Canvas, Rect } from '@shopify/react-native-skia'
 import { View, StyleSheet } from 'react-native'
-import {
-    transformGameSpacePositionToScreenSpacePosition,
-    useGameArea,
-    useGameAreaScaleFactor,
-} from '../utils/gameArea'
+import { useGameArea, useGameAreaScaleFactor } from '../utils/gameArea'
 import { useCallback, useEffect } from 'react'
 import { GameState, UUID } from '../types'
-import {
-    GAME_AREA_BORDER_WIDTH,
-    LINE_STROKE_WIDTH,
-    PLAYER_COLORS,
-    PLAYER_SIZE,
-} from '../utils/constants'
+import { GAME_AREA_BORDER_WIDTH } from '../utils/constants'
 import { GestureDetector } from 'react-native-gesture-handler'
 import { useChangeDirectionGesture } from '../utils/useChangeDirectionGesture'
 import { Player } from './Player'
 import { useServerGameState } from '../utils/serverGameState'
-import {
-    convertUnitVectorToDegrees,
-    convertUnitVectorToRadians,
-} from '../utils/geometry'
 
 type Props = {
     userId: UUID
@@ -51,55 +38,13 @@ export function GameCanvas({ gameState }: Props) {
 
     const renderPlayers = useCallback(() => {
         return playerIds.map((id, index) => {
-            const positionX = Selector(players, (p) =>
-                p[id]
-                    ? transformGameSpacePositionToScreenSpacePosition(
-                          p[id],
-                          gameAreaScaleFactor
-                      ).x -
-                      (PLAYER_SIZE - LINE_STROKE_WIDTH) / 2
-                    : 0
-            )
-
-            const positionY = Selector(players, (p) =>
-                p[id]
-                    ? transformGameSpacePositionToScreenSpacePosition(
-                          p[id],
-                          gameAreaScaleFactor
-                      ).y -
-                      (PLAYER_SIZE - LINE_STROKE_WIDTH) / 2
-                    : 0
-            )
-
-            const origin = Selector(players, (p) =>
-                p[id]
-                    ? transformGameSpacePositionToScreenSpacePosition(
-                          p[id],
-                          gameAreaScaleFactor
-                      )
-                    : { x: 0, y: 0 }
-            )
-
-            const transform = Selector(players, (p) => [
-                {
-                    rotate: p[id]
-                        ? convertUnitVectorToRadians({
-                              x: p[id].angleUnitVectorX,
-                              y: p[id].angleUnitVectorY,
-                          }) +
-                          Math.PI / 2
-                        : 0,
-                },
-            ])
-
             return (
                 <Player
                     key={`player_${index}`}
-                    x={positionX}
-                    y={positionY}
-                    origin={origin}
-                    color={PLAYER_COLORS[index % PLAYER_COLORS.length]}
-                    transform={transform}
+                    players={players}
+                    playerId={id}
+                    playerIndex={index}
+                    gameAreaScaleFactor={gameAreaScaleFactor}
                 />
             )
         })
--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,12 +1,85 @@
 import {
-    AnimatedProps,
     ImageSVG,
-    ImageSVGProps,
+    SkiaMutableValue,
+    Selector,
 } from '@shopify/react-native-skia'
 import { PLAYER_SVG } from '../utils/drawing'
+import { Player as PlayerType, UUID } from '../types'
+import { transformGameSpacePositionToScreenSpacePosition } from '../utils/gameArea'
+import {
+    LINE_STROKE_WIDTH,
+    PLAYER_COLORS,
+    PLAYER_SIZE,
+} from '../utils/constants'
+import { convertUnitVectorToRadians } from '../utils/geometry'
 
-type Props = Omit<AnimatedProps<ImageSVGProps>, 'svg'>
+type Props = {
+    players: SkiaMutableValue<Record<UUID, PlayerType>>
+    playerId: UUID
+    playerIndex: number
+    /**
+     * Components rendered a skia context can't get use the safe area hook
+     */
+    gameAreaScaleFactor: number
+}
 
-export function Player(props: Props) {
-    return <ImageSVG {...props} svg={PLAYER_SVG} />
+export function Player({
+    players,
+    playerId,
+    playerIndex,
+    gameAreaScaleFactor,
+}: Props) {
+    const y = Selector(players, (p) =>
+        p[playerId]
+            ? transformGameSpacePositionToScreenSpacePosition(
+                  p[playerId],
+                  gameAreaScaleFactor
+              ).x -
+              (PLAYER_SIZE - LINE_STROKE_WIDTH) / 2
+            : 0
+    )
+
+    const x = Selector(players, (p) =>
+        p[playerId]
+            ? transformGameSpacePositionToScreenSpacePosition(
+                  p[playerId],
+                  gameAreaScaleFactor
+              ).y -
+              (PLAYER_SIZE - LINE_STROKE_WIDTH) / 2
+            : 0
+    )
+
+    const origin = Selector(players, (p) =>
+        p[playerId]
+            ? transformGameSpacePositionToScreenSpacePosition(
+                  p[playerId],
+                  gameAreaScaleFactor
+              )
+            : { x: 0, y: 0 }
+    )
+
+    const transform = Selector(players, (p) => [
+        {
+            rotate: p[playerId]
+                ? convertUnitVectorToRadians({
+                      x: p[playerId].angleUnitVectorX,
+                      y: p[playerId].angleUnitVectorY,
+                  }) +
+                  Math.PI / 2
+                : 0,
+        },
+    ])
+
+    return (
+        <ImageSVG
+            svg={PLAYER_SVG}
+            x={y}
+            y={x}
+            width={PLAYER_SIZE}
+            height={PLAYER_SIZE}
+            origin={origin}
+            transform={transform}
+            color={PLAYER_COLORS[playerIndex % PLAYER_COLORS.length]}
+        />
+    )
 }
